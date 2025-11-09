dba1da1577daf982e425ca837541d7bcbc79196c
danielaghidini <daniela.ghidini@gmail.com>
2025-11-09 10:21:15 -0300

    fix: update modInfo.json title and subtitle for clarity; set basename in BrowserRouter for production

--- src/main.jsx
+++ src/main.jsx
@@ -9,9 +9,11 @@ import Dialogues from "./pages/Dialogues";
 import Quests from "./pages/Quests";
 import About from "./pages/About";
 
+const baseName = import.meta.env.MODE === "production" ? "/bjorn-wiki" : "/";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={baseName}>
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
